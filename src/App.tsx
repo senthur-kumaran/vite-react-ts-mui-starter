dd3d675d25177fd03e2798a84dc7dd7293a8c11e
Senthur Kumaran <senthur@Senthurs-MacBook-Pro.local>
2022-05-14 08:18:23 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Album from "./components/album/Album"
+import Blog from "./components/blog/Blog"
+import Checkout from "./components/checkout/Checkout"
+import Dashboard from "./components/dashboard/Dashboard"
+import Pricing from "./components/pricing/Pricing"
+import SignInSide from "./components/sign-in-side/SignInSide"
+import SignIn from "./components/sign-in/SignIn"
+import SignUp from "./components/sign-up/SignUp"
+import StickyFooter from "./components/sticky-footer/StickyFooter"
+
+function App() {
+  return <Dashboard />
+}
+
+export default App
